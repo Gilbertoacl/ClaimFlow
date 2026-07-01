97167c5c524c1ae8cd4477eb2c84581240dc875e
Gilbertoacl <betoacl@hotmail.com>
2026-07-01 19:38:24 -0300

    Commit Inicial do sistema.

---
+++ src/main/java/dev/gilbertoacl/claimflow_api/ClaimflowApiApplication.java
@@ -0,0 +1,13 @@
+package dev.gilbertoacl.claimflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gilbertoacl/claimflow_api/ClaimflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gilbertoacl.claimflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

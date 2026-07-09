2ef0fcfdc0b83c8e1036823297db2689c5abf3d1
Gilbertoacl <betoacl@hotmail.com>
2026-07-09 20:13:12 -0300

    feat(Exception) Criado as exceptions personalizadas do sistema

---
+++ src/main/java/dev/gilbertoacl/claimflow_api/shared/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package dev.gilbertoacl.claimflow_api.shared.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}

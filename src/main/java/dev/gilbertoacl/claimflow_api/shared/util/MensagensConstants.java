ef906dc92b72861ebc24047b4f769c325906e3de
Gilbertoacl <betoacl@hotmail.com>
2026-07-20 17:28:29 -0300

    feat(Utils) Adicionado novas mensagens do sistema

--- src/main/java/dev/gilbertoacl/claimflow_api/shared/util/MensagensConstants.java
+++ src/main/java/dev/gilbertoacl/claimflow_api/shared/util/MensagensConstants.java
@@ -6,8 +6,13 @@ public class MensagensConstants {
     public static final String APOLICE_NAO_ENCONTRADA = "Apólice não encontrada.";
     public static final String APOLICE_JA_EXISTE = "Apólice já existe.";
     public static final String FIM_VIGENCIA_DEVE_SER_POSTERIOR_A_INICIO = "Fim de vigência deve ser posterior ao início.";
-    public static final String APOLICE_JA_CANCELADA = "Apólice ja Cancelada" ;
+    public static final String APOLICE_JA_CANCELADA = "Apólice ja Cancelada." ;
     public static final String APOLICE_VENCIDA_NAO_PODE_CANCELAR = "Apólice Vencida. Não pode Cancelar.";
+    public static final String APOLICE_INATIVA_NAO_PODE_ABRIR_SINISTRO = "Apólice Vencida. Não pode abrir Sinistro.";
+    public static final String SINISTRO_FORA_DE_VIGTENCIA = "A ocorrência está fora da vigência da apólice.";
+    public static final String VALOR_SOLICITADO_MAIOR_QUE_SEGURADO = "O valor solicitado não pode ser maior que o valor segurado.";
+    public static final String LIMITE_DIARIO_SINISTRO_ATINGIDO = "Limite de sinistros diário atingido para o tipo de apolice: ";
+    public static final String SINISTRO_NAO_ENCONTRADO = "Sinistro não encontrado";
 
     private MensagensConstants() {
         throw new UnsupportedOperationException(CLASSE_UTILIDADE_NAO_INSTANCIADA);
